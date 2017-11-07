a1bdfd4fa26b11799526132d31c0f6dca651dffa
mmmurphy133 <32489087+mmmurphy133@users.noreply.github.com>
2017-11-07 10:56:55 -0500

    Update leafletChoropleth.js

--- leafletChoropleth.js
+++ leafletChoropleth.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 // initialize the map
-var coords = [39, -76.6]; // map center point
+var coords = [38.9, -76.78]; // map center point
 var zoom = 8.5; // map zoom level lower number is out higher number is in
 var map = L.map('map').setView(coords, zoom); // put it all together
 
