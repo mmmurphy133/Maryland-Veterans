301561319337f451a7caaba62b13f141512ce0c7
mmmurphy133 <32489087+mmmurphy133@users.noreply.github.com>
2017-11-07 09:51:58 -0500

    Update leafletChoropleth.js

--- leafletChoropleth.js
+++ leafletChoropleth.js
@@ -104,7 +104,7 @@ var legend = L.control({
 legend.onAdd = function(map) {
 
   var div = L.DomUtil.create('div', 'info legend'),
-    grades = [0, 1000, 5500, 13000, 33000, 60000],
+    grades = [0, 1000, 5500, 13000, 33000],
     labels = [],
     from, to;
 
