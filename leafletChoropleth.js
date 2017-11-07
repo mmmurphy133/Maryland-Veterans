49a5797eba86dff39e9da58f36c71dad209c55d4
mmmurphy133 <32489087+mmmurphy133@users.noreply.github.com>
2017-11-07 08:17:35 -0500

    https

--- leafletChoropleth.js
+++ leafletChoropleth.js
@@ -6,7 +6,7 @@ var map = L.map('map').setView(coords, zoom); // put it all together
 
 ////////////////////////////////////////////////////////////////////////////////
 // load a basemap tile layer
-L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
   maxZoom: 60,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Data by <a href="https://github.com/mmmurphy133">Meghan Murphy</a>'
 }).addTo(map);
